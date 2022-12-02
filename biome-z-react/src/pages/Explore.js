8ae3f4f89e51adf0a8f2f86c6c0c632a21c4f7cc
unknown <ewhite9@gulls.salisbury.edu>
2022-12-02 01:33:11 -0500

    Add Explore page VennDiagram

--- biome-z-react/src/pages/Explore.js
+++ biome-z-react/src/pages/Explore.js
@@ -1,5 +1,16 @@
+//import React, {Component} from 'react'
+import React from 'react'
+//import { render } from 'react-dom'
+import VennDiagram from '../Components/VennDiagram'
+import "./Explore.css";
 function ExplorePage() {
-    return <div>Explore Page</div>
+    return (
+    <div className="container" >
+        <h1>
+            <VennDiagram />
+        </h1>
+    </div>
+    )
 }
 
 export default ExplorePage
