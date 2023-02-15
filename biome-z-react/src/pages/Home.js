ce96a9bebd23f2edeb66fda8f8d70d6b9996a5b6
SBlaxberg1 <rachamimb@gmail.com>
2023-02-15 08:28:00 -0500

    Added media player (WIP)

--- biome-z-react/src/pages/Home.js
+++ biome-z-react/src/pages/Home.js
@@ -6,12 +6,19 @@ function HomePage() {
             <header className="header">
                 <h1 className="header-title">WELCOME TO THE BIOME-Z DATABASE.</h1>
             </header>
+
             <main className="main">
                 <p className="large-text"> OVERVIEW </p>
                 <p className="small-text"> Biome-Z (Biopsychosocial Index of Microbiome Ecologies - Zotero) is an interactive research tool that categorizes information using the BPSE Framework.
                 Explore the database <a className="link-text" href="/Explore"> here </a> or use the search bar to find a specific category or article. The BPSE Framework sorts specific research data into meaningful subtopics (Biological, Psychological and Social Evironments), making it simple to find obscure articles.
                  </p>
             </main>
+
+            <main className="videoPlayer">
+                <iframe title="vimeo-player" src="https://player.vimeo.com/video/501597571?h=629fee29e7" width="1300" height="600" frameborder="0" allowfullscreen="true"></iframe>
+                <p className="video-text"> How Does The Framework Work? </p>
+            </main>
+
             <main className="main">
                 <p className="large-text"> FEATURES </p>
                 <p className="small-text">
@@ -22,6 +29,7 @@ function HomePage() {
                     </ul>
                 </p>
             </main>
+
         </div>
     );
 }
