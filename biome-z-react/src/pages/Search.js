91f9646392e54d32bd2e4b8856951b68bcfc01b2
SBlaxberg1 <rachamimb@gmail.com>
2023-03-01 17:56:59 -0500

    Added search functionality (hooked to backend) and formatted accordingly

--- biome-z-react/src/pages/Search.js
+++ biome-z-react/src/pages/Search.js
@@ -1,98 +1,79 @@
-import React, { useState, useEffect } from 'react';
 import "./Search.css";
-//import searchInput from "../Components/Navbar";
+import React, { useState, useEffect } from 'react';
 
+function SearchPage() {
+    const [query, setQuery] = useState('');
+    const [results, setResults] = useState([]);
 
-function Search() {
-    
-  const [data, setData] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('http://localhost:5000/post-json', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ q: query })
+            });
+            const data = await response.json();
+            setResults(data);
+        };
 
-
-  useEffect(() => {
-      async function fetchData() {
-          const response = await fetch('http://localhost:5000/post-json',
-            { 
-                params: {searchTerm: this.target.value} 
-            }
-          );
-          const json = await response.json();
-          setData(json);
-      }
-      fetchData();
-  }, []);
-
-  const handleSearchChange = event => (
-      setSearchTerm ({
-        query: event.target.value
-    
-    }), () => {
-        if (event.target.value) {
-            this.useEffect()
+        if (query !== '') {
+            fetchData();
         }
-    }
-  );
+    }, [query]);
 
-  const filteredData = data.filter(row =>
-      (row.articleName.toLowerCase().includes(searchTerm.toLowerCase()) || row.authorName.toLowerCase().includes(searchTerm.toLowerCase()))
-  );
-    
-  return (
-      <>
-        <div className="abc" >
-          <form>
-              <label htmlFor="search">Search:</label>
-              <input
-                  type="text"
-                  id="Search2"
-                  value={searchTerm }
-                  onChange={handleSearchChange}
-              />
-          </form>
+    const handleInputChange = (event) => {
+        setQuery(event.target.value);
+    };
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            setQuery(event.target.value);
+        }
+    };
+
+    return (
+        <div className="container2">
+            <div className="searchBar">
+                <input className="searchText"
+                    type="text"
+                    value={query}
+                    onChange={handleInputChange}
+                    onKeyPress={handleKeyPress}
+                    />
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Publication Year</th>
+                        <th>Author</th>
+                        <th>Title</th>
+                        <th>Publication Title</th>
+                        <th>ISSN</th>
+                        <th>DOI</th>
+                        <th>URL</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {results.map((result) => (
+                        <tr key={result._id}>
+                            <td>{result.pubYear}</td>
+                            <td>{result.author}</td>
+                            <td>{result.title}</td>
+                            <td>{result.pubTitle}</td>
+                            <td>{result.issn}</td>
+                            <td>{result.doi}</td>
+                            <td><a href={result.url} target="_blank">Link</a></td>
+                            <td>{result.date}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
         </div>
-        <div className="container2" >
-          <table>
-              <thead>
-                  <tr>
-                      <th>Type</th>
-                      <th>Publish Year</th>
-                      <th>Author</th>
-                      <th>Title</th>
-                      <th>issn</th>
-                      <th>DOI</th>
-                      <th>URL</th>
-                      <th>Abstract</th>
-                      <th>Date</th>
-                      <th>Issue</th>
-                      <th>Volume</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  {filteredData.map(row => (
-                      <tr key={row.id}>
-                          <td>{row.pubYear}</td>
-                          <td>{row.author}</td>
-                          <td>{row.title}</td>
-                          <td>{row.issn}</td>
-                          <td>{row.doi}</td>
-                          <td>{row.url}</td>
-                          <td>{row.abstract}</td>
-                          <td>{row.date}</td>
-                          <td>{row.issue}</td>
-                          <td>{row.volume}</td>
-                          <td>{row.authorName}</td>
-                          <td>{row.doi}</td>
-                          <td>{row.articleName}</td>
-                          <td>{row.authorName}</td>
-                          <td>{row.doi}</td>
-                      </tr>
-                  ))}
-              </tbody>
-          </table>
-          </div>
-      </>
-  );
+    );
 }
 
-
-export default Search
+export default SearchPage;
