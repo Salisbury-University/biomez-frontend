8a6df106da539bf55c5d1f78af911e6110684e67
SBlaxberg1 <rachamimb@gmail.com>
2023-03-12 15:23:49 -0400

    Fixed Footer Rendering issue on Search Results page

--- biome-z-react/src/App.js
+++ biome-z-react/src/App.js
@@ -5,29 +5,31 @@ import HomePage from "./pages/Home"
 import ExplorePage from "./pages/Explore"
 import SearchPage from "./pages/Search"
 import Footer from "./Components/Footer";
-import { Route, Routes } from "react-router-dom"
-export default function App()
-{
+import { Route, Routes, useLocation } from "react-router-dom"
+export default function App() {
+    const location = useLocation();
+    const hideFooter = location.pathname === "/Search";
 
-  return(
-    
-    <div  className="divScroll" >
-      <Navbar />
-      <div className="Navbar"> </div>
-      
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<HomePage />}/>
-          <Route path="/Home" element={<HomePage />}/>
-          <Route path="/About" element={<AboutPage />}/>
-          <Route path="/Explore" element={<ExplorePage />}/>
-          <Route path="/Search" element={<SearchPage />}/>
-        </Routes>
-      </div>
+    return (
+        <div className="divScroll" >
+            <Navbar />
+            <div className="Navbar"> </div>
 
-     <div className="footer">
-          <Footer />
-     </div>
-    </div>
-    );  
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/Home" element={<HomePage />} />
+                    <Route path="/About" element={<AboutPage />} />
+                    <Route path="/Explore" element={<ExplorePage />} />
+                    <Route path="/Search" element={<SearchPage />} />
+                </Routes>
+            </div>
+
+            {!hideFooter && (
+                <div className="footer">
+                    <Footer />
+                </div>
+            )}
+        </div>
+    );
 }
